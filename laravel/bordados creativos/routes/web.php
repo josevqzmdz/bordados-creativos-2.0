d031eb18bef2233b781d11b45f244038b4a5220d
josevqzmdz <notanothergenericaddress@gmail.com>
2021-05-14 21:49:53 -0500

    quinto? no se la vdd jajajaja

--- laravel/bordados creativos/routes/web.php
+++ laravel/bordados creativos/routes/web.php
@@ -52,3 +52,11 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
